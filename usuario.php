14d97bee074a1c18b8728c3f12f422903f64f79e
viniciuslucio <107164537+viniciuslucio@users.noreply.github.com>
2024-06-13 17:55:56 -0300

    Add files via upload

--- usuario.php
+++ usuario.php
@@ -11,8 +11,62 @@
     <title>Olá, mundo!</title>
   </head>
   <body>
-    <h1>Recomenda Filme</h1>
-    
+    <h1 class="text-center">Recomenda Filme</h1>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="usuario.php">Início</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sobre Nós</a>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Histórico</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Ajuda</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Categorias
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Ação</a>
+          <a class="dropdown-item" href="#">Terror</a>
+          <a class="dropdown-item" href="#">Romance</a>
+          <a class="dropdown-item" href="#">Comédia</a>
+          <a class="dropdown-item" href="#">Suspense</a>
+          <a class="dropdown-item" href="#">Para Toda a Família</a>
+          <a class="dropdown-item" href="#">Adulto</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Mais...</a>
+        </div>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Raking(Mais Vistos)
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Harry Potter</a>
+          <a class="dropdown-item" href="#">Batman: O Cavaleiro Das Trevas</a>
+          <a class="dropdown-item" href="#">Coringa</a>
+          <a class="dropdown-item" href="#">Batman Begins</a>
+          <a class="dropdown-item" href="#">Vingadores: A Era de Ultron</a>
+          <a class="dropdown-item" href="#"></a>
+          <a class="dropdown-item" href="#">Adulto</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Mais...</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+    </form>
+  </div>
+</nav>
 
     <!-- JavaScript (Opcional) -->
     <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
